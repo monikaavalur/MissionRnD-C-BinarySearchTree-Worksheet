a7de6edb45d92df33f2f2e3a6496af3d8527f286
monikaavalur <monikaavalur@missionrnd.com>
2016-03-29 09:49:07 +0530

    Submit Commit

--- src/BSTClosestLeafDistance.cpp
+++ src/BSTClosestLeafDistance.cpp
@@ -38,8 +38,51 @@ struct node{
   int data;
   struct node *right;
 };
-
+void leafnode(struct node*, int, int* );
+int parentnode(struct node* , struct node* , int*);
 int get_closest_leaf_distance(struct node *root, struct node *temp)
 {
-  return -1;
+	if (root == NULL || temp == NULL)
+		return -1;
+	if (root->left == NULL&&root->right == NULL)
+		return 0;
+	int minDist = INT_MAX;
+	leafnode(temp, 0, &minDist);
+	parentnode(root, temp, &minDist);
+	return minDist;
+  
+}
+void leafnode(struct node *root, int level, int *minDist)
+{
+	if (root == NULL)
+		return;
+	if (root->left == NULL && root->right == NULL)
+	{
+		if (level < (*minDist))
+			*minDist = level;
+		return;
+	}
+	leafnode(root->left, level + 1, minDist);
+	leafnode(root->right, level + 1, minDist);
+}
+
+
+int parentnode(struct node * root, struct node *temp, int *minDist)
+{
+	if (root == NULL) return -1;
+	if (root == temp) return 0;
+	int left = parentnode(root->left, temp, minDist);
+	if (left != -1)
+	{
+		leafnode (root->right, left + 2, minDist);
+		return left + 1;
+	}
+	int right = parentnode(root->right, temp, minDist);
+	if (right != -1)
+	{
+		leafnode(root->left, right + 2, minDist);
+		return right + 1;
+	}
+
+	return -1;
 }
